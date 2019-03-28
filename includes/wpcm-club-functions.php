65c7ee97c0d610d6bdd58c9b2e9ec867f1ab1380
Davey Jacobson <daveyjake21@gmail.com>
2019-03-28 16:30:43 -0600

    Upgrade to 2.0.5
    Updated to WPClubManager version 2.0.5

--- includes/wpcm-club-functions.php
+++ includes/wpcm-club-functions.php
@@ -7,58 +7,47 @@
  * @author 		ClubPress
  * @category 	Core
  * @package 	WPClubManager/Functions
- * @version     1.4.0
+ * @version     2.0.5
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-function wpcm_head_to_heads( $post ) {
-
+function wpcm_head_to_heads( $post_id ) {
+	
 	$club = get_default_club();
-
-	// get matches
-	$query_args = array(
-		'numberposts' => '-1',
-		'order' => 'ASC',
-		'orderby' => 'post_date',
+	
+	$args = array(
 		'post_type' => 'wpcm_match',
-		'post_status' => array('publish'),
-		'posts_per_page' => '-1'
-	);
-
-
-	$query_args['meta_query'] = array(
-		'relation' => 'OR',
-		array(
-			'relation' => 'AND',
-			array (
-				'key' => 'wpcm_home_club',
-				'value' => $club,
+		'post_per_page' => -1,
+		'meta_query' => array(
+			array(
+				'relation' => 'OR',
+				array(
+					'key' => 'wpcm_home_club',
+					'value' => $post_id
+				),
+				array(
+					'key' => 'wpcm_away_club',
+					'value' => $post_id
+				)
 			),
-			array (
-				'key' => 'wpcm_away_club',
-				'value' => $post,
-			),
-		),
-		array(
-			'relation' => 'AND',
-			array (
-				'key' => 'wpcm_home_club',
-				'value' => $post,
-			),
-			array (
-				'key' => 'wpcm_away_club',
-				'value' => $club,
+			array(
+				'relation' => 'OR',
+				array(
+					'key' => 'wpcm_home_club',
+					'value' => $club
+				),
+				array(
+					'key' => 'wpcm_away_club',
+					'value' => $club
+				),
 			),
 		)
 	);
 
-	$matches = get_posts( $query_args );
-
-	wp_reset_postdata();
+	$matches = get_posts( $args );
 
 	return $matches;
-
 }
 
 function wpcm_head_to_head_count( $matches ) {
@@ -70,28 +59,31 @@ function wpcm_head_to_head_count( $matches ) {
 	$count = 0;
 	foreach( $matches as $match ) {
 
-		$count ++;
-		$home_club = get_post_meta( $match->ID, 'wpcm_home_club', true );
-		$home_goals = get_post_meta( $match->ID, 'wpcm_home_goals', true );
-		$away_goals = get_post_meta( $match->ID, 'wpcm_away_goals', true );
+		if( get_post_meta( $match->ID, '_wpcm_postponed', true ) != '1' && get_post_meta( $match->ID, '_wpcm_walkover', true ) == '' ) {
 
-		if ( $home_goals == $away_goals ) {
-			$draws ++;
-		}
+			$count ++;
+			$home_club = get_post_meta( $match->ID, 'wpcm_home_club', true );
+			$home_goals = get_post_meta( $match->ID, 'wpcm_home_goals', true );
+			$away_goals = get_post_meta( $match->ID, 'wpcm_away_goals', true );
 
-		if ( $club == $home_club ) {
-			if ( $home_goals > $away_goals ) {
-				$wins ++;
+			if ( $home_goals == $away_goals ) {
+				$draws ++;
 			}
-			if ( $home_goals < $away_goals ) {
-				$losses ++;
-			}
-		} else {
-			if ( $home_goals > $away_goals ) {
-				$losses ++;
-			}
-			if ( $home_goals < $away_goals ) {
-				$wins ++;
+
+			if ( $club == $home_club ) {
+				if ( $home_goals > $away_goals ) {
+					$wins ++;
+				}
+				if ( $home_goals < $away_goals ) {
+					$losses ++;
+				}
+			} else {
+				if ( $home_goals > $away_goals ) {
+					$losses ++;
+				}
+				if ( $home_goals < $away_goals ) {
+					$wins ++;
+				}
 			}
 		}
 
@@ -102,6 +94,30 @@ function wpcm_head_to_head_count( $matches ) {
 	$outcome['draws'] = $draws;
 	$outcome['losses'] = $losses;
 
-	return $outcome;
+	return apply_filters( 'wpcm_head_to_head_count', $outcome, $matches );
 
 }
+
+function get_club_venue( $post ) {
+
+	$id = get_the_terms( $post, 'wpcm_venue' );
+	
+	if ( is_array( $id ) ) {
+		$venues = reset($id);
+		$t_id = $venues->term_id; 
+		$venue_meta = get_option( "taxonomy_term_$t_id" );
+		$venue['id'] = $t_id;
+		$venue['name'] = $venues->name;
+		if( is_array( $venue_meta ) ) {
+			if( array_key_exists('wpcm_address', $venue_meta) ) {
+				$venue['address'] = $venue_meta['wpcm_address'];
+			}
+			if( array_key_exists('wpcm_capacity', $venue_meta) ) {
+				$venue['capacity'] = $venue_meta['wpcm_capacity'];
+			}
+		}
+		$venue['description'] = $venues->description;
+	}
+	 return $venue;
+}
+	
